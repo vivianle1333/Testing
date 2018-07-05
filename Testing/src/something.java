29d4525a73c9cf2ac9c2c9300ab9bea5879c3b90
vivianle1333 <vivian.le1333@hotmail.com>
2018-07-05 16:27:59 -0400

    added a void function called doingstuff()

--- Testing/src/something.java
+++ Testing/src/something.java
@@ -8,8 +8,13 @@ public class something {
 		System.out.println(a);
 	}
 	
+	private static void doingstuff() {
+		System.out.println(1+1);
+	}
+	
 	public static void main(String[] args) {
 		something b = new something();
 		System.out.println("this is cool");
+		doingstuff();
 	}
 }

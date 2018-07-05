12bcc695ecc1e3b0c40df8caf605cd76941561be
vivianle1333 <vivian.le1333@hotmail.com>
2018-07-05 16:07:59 -0400

    testing this git thing

---
+++ Testing/src/something.java
@@ -0,0 +1,15 @@
+import java.lang.System;
+
+
+public class something {
+	public something() {
+		String a = "hello somthing";
+		
+		System.out.println(a);
+	}
+	
+	public static void main(String[] args) {
+		something b = new something();
+		System.out.println("this is cool");
+	}
+}
